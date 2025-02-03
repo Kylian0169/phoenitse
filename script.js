cac48e459d0d6883e61d6265cab17123b7da715b
Kylian <kylian.lombard@wanadoo.fr>
2025-02-03 03:02:03 +0100

    Modifications des boutons selon l'appareil utilisé, ajout d'images des différents events chaque jour et insertion en html. Changement du nombre pixels de décalage pour chaque bloc de jour et changement du nombre d'évènements affichés sur l'appareil dans le js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentSlide = 0;
 function slide(direction) {
   const slider = document.querySelector(".slider");
   const totalBanners = slider.children.length;
-  const bannersPerView = 3;
+  const bannersPerView = 1;
 
   // Calculate the max slide index
   const maxSlideIndex = totalBanners - bannersPerView;
@@ -11,7 +11,7 @@ function slide(direction) {
   // Update the slide position
   currentSlide = Math.min(Math.max(currentSlide + direction, 0), maxSlideIndex);
 
-  const offset = (currentSlide * -100) / bannersPerView; // Adjust for 3 banners
+  const offset = (currentSlide * -70) / bannersPerView; // Adjust for 3 banners
   slider.style.transform = `translateX(${offset}%)`;
 }
 
